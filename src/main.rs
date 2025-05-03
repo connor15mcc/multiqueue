6924a9ca0f6f4f19f53b733ea63516e7aace9ac8
Connor McCarthy <connor15mcc@gmail.com>
2025-05-03 13:04:53 -0400

    manual multiqueue impl
    I had already tried w/ `async_channel` crate but found that to be
    insufficient for what I want

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+#[derive(Debug)]
+struct Task {
+    inner: String,
+}
+
+impl Task {
+    fn new(s: &str) -> Self {
+        Task {
+            inner: s.to_string(),
+        }
+    }
+}
+
+struct MultiQueue<T> {
+    vec: Vec<T>,
+}
+
+impl<T> MultiQueue<T> {
+    fn new(vec: Vec<T>) -> Self {
+        MultiQueue { vec }
+    }
+
+    fn get<P>(&mut self, pred: P) -> Option<T>
+    where
+        P: FnMut(&T) -> bool,
+    {
+        let idx = self.vec.iter().position(pred)?;
+        let elt = self.vec.swap_remove(idx);
+        Some(elt)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = Task::new("a");
+    let b = Task::new("b");
+    let c = Task::new("c");
+
+    let mut q = MultiQueue::new(vec![a, b, c]);
+
+    let task = q.get(|t| t.inner == "a");
+    println!("Got task! {:?}", task);
+    let task = q.get(|t| t.inner == "a");
+    println!("Got task! {:?}", task);
 }
